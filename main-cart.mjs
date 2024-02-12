2d851d7a09a084a876ceb69507fc0fe127d0f31e
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-02-12 13:08:35 +0530

    wip

--- main-cart.mjs
+++ main-cart.mjs
@@ -1,65 +1,54 @@
 import { create } from "./cards.mjs";
 
-function setListItems(data) {
+const items = JSON.parse(localStorage.getItem("itemList"));
+const cardGroup = document.getElementById("content");
+const total = document.getElementById("total");
+let itemTotal = 0;
+
+function updateListItems(data) {
   localStorage.setItem("itemList", JSON.stringify(data));
 }
 
-const items = JSON.parse(localStorage.getItem("itemList"));
-let total = document.getElementById("total");
-let itemTotal = 0;
-const cardGroup = document.getElementById("content");
-
 for (let i = 0; i < 8; i++) {
   create(i, cardGroup, items);
-}
-
-for (let i = 0; i < 8; i++) {
+  let cartMinusbtn = document.getElementsByClassName("cart-minusbtn".concat(i));
+  let cartAddbtn = document.getElementsByClassName("cart-addbtn".concat(i));
+  let cartbtn = document.getElementsByClassName("cart-btn".concat(i));
+  let cartGroupItem = document.getElementById("cardGroupItem".concat(i));
   itemTotal += items[i]["quantity"] * Number(items[i]["price"]);
   total.textContent = "Your Total \u20b9" + itemTotal;
-  let y = document.getElementsByClassName("cart-minusbtn".concat(i));
-  y[0].style.display = "block";
-  let x = document.getElementsByClassName("cart-addbtn".concat(i));
-  x[0].style.display = "block";
-  let w = document.getElementsByClassName("cart-btn".concat(i));
-  w[0].textContent = items[i]["quantity"];
+  cartMinusbtn[0].style.display = "block";
+  cartAddbtn[0].style.display = "block";
+  cartbtn[0].textContent = items[i]["quantity"];
 
   if (items[i]["quantity"] < 1) {
-    let x = document.getElementById("cardGroupItem".concat(i));
-    x.style.display = "none";
+    let cartGroupItem = document.getElementById("cardGroupItem".concat(i));
+    cartGroupItem.style.display = "none";
   }
 }
 
 cardGroup.addEventListener("click", function (event) {
   for (let i = 0; i < 8; i++) {
+    let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
+    let cardGroupItem = document.getElementById("cardGroupItem".concat(i));
+
     if (event.target.classList.contains("cart-addbtn".concat(i))) {
       items[i]["quantity"]++;
-      let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
+      updateListItems(items);
       addtocartbtn[0].textContent = items[i]["quantity"];
       itemTotal += Number(items[i]["price"]);
       total.textContent = "Your Total \u20b9" + itemTotal;
-      setListItems(items);
     }
-  }
-});
 
-cardGroup.addEventListener("click", function (event) {
-  for (let i = 0; i < 8; i++) {
     if (event.target.classList.contains("cart-minusbtn".concat(i))) {
       let zero = --items[i]["quantity"];
-      let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
       addtocartbtn[0].textContent = zero;
       itemTotal -= Number(items[i]["price"]);
       total.textContent = "Your Total \u20b9" + itemTotal;
-      if (zero <= 0) {
-        zero = 0;
-      } else {
-        addtocartbtn[0].textContent = zero;
-      }
-      setListItems(items);
+      updateListItems(items);
     }
     if (items[i]["quantity"] < 1) {
-      let x = document.getElementById("cardGroupItem".concat(i));
-      x.style.display = "none";
+      cardGroupItem.style.display = "none";
     }
   }
 });
--- main.mjs
+++ main.mjs
@@ -1,6 +1,11 @@
 import data from "./data.json" assert { type: "json" };
 import { create } from "./cards.mjs";
 
+const items = JSON.parse(localStorage.getItem("itemList"));
+const cardGroup = document.getElementById("content");
+const cartSum = document.getElementById("cart-num");
+let cartQuantity = 0;
+
 function setList(data) {
   if (typeof localStorage !== "undefined") {
     if (!localStorage.getItem("itemList")) {
@@ -11,114 +16,95 @@ function setList(data) {
   }
 }
 
-function setListItems(data) {
+function updateListItems(data) {
   localStorage.setItem("itemList", JSON.stringify(data));
 }
 
-setList(data);
-const items = JSON.parse(localStorage.getItem("itemList"));
-const cardGroup = document.getElementById("content");
-let cartQuantity = 0;
-const cartSum = document.getElementById("cart-num");
+function theme() {
+  let white = "white";
+  let black = "#3e3e42";
+  document.body.style.backgroundColor = white;
+  let themeDiv = document.getElementsByClassName("theme");
+  let theme = document.getElementById("theme-img");
 
-let white = "white";
-let black = "#3e3e42";
-document.body.style.backgroundColor = white;
-let themeDiv = document.getElementsByClassName("theme");
-let theme = document.getElementById("theme-img");
-console.log(theme);
-
-themeDiv[0].addEventListener("click", function () {
-  if (document.body.style.backgroundColor == white) {
-    document.body.style.backgroundColor = black;
-    theme.setAttribute("src", "images//sun-solid.svg");
-  } else {
-    document.body.style.backgroundColor = white;
-    theme.setAttribute("src", "images//moon-solid.svg");
-  }
-});
-
-for (let i = 0; i < 8; i++) {
-  create(i, cardGroup, items);
+  themeDiv[0].addEventListener("click", function () {
+    if (document.body.style.backgroundColor === white) {
+      document.body.style.backgroundColor = black;
+      theme.setAttribute("src", "images//sun-solid.svg");
+    } else {
+      document.body.style.backgroundColor = white;
+      theme.setAttribute("src", "images//moon-solid.svg");
+    }
+  });
 }
 
-for (let i = 0; i < 8; i++) {
-  cartQuantity += items[i]["quantity"];
+function checkQuantity() {
   if (cartQuantity >= 1) {
     cartSum.textContent = cartQuantity;
     cartSum.style.display = "inline-block";
+  } else {
+    cartSum.style.display = "none";
   }
+}
+
+theme();
+setList(data);
+
+for (let i = 0; i < 8; i++) {
+  create(i, cardGroup, items);
+  cartQuantity += items[i]["quantity"];
+  checkQuantity();
+}
+
+function centerCartBtn(cartAddbtn, cartMinusbtn, addtocartbtn, i) {
+  cartAddbtn.style.display = "block";
+  cartMinusbtn.style.display = "block";
+  items[i]["quantity"]++;
+  updateListItems(items);
+  addtocartbtn.textContent = items[i]["quantity"];
+  cartQuantity++;
+  checkQuantity();
+}
+
+function addCartBtn(addtocartbtn, i) {
+  items[i]["quantity"]++;
+  addtocartbtn.textContent = items[i]["quantity"];
+  updateListItems(items);
+  cartQuantity++;
+  checkQuantity();
+}
+
+function minusCartBtn(addtocartbtn, cartAddbtn, cartMinusbtn, i) {
+  let zero = --items[i]["quantity"];
+  updateListItems(items);
+  addtocartbtn.textContent = zero;
+  if (zero <= 0) {
+    addtocartbtn.textContent = "Add to Cart";
+    cartAddbtn.style.display = "none";
+    cartMinusbtn.style.display = "none";
+  }
+  cartQuantity--;
+  checkQuantity();
 }
 
 cardGroup.addEventListener("click", function (event) {
   for (let i = 0; i < 8; i++) {
+    let cartMinusbtn = document.getElementsByClassName(
+      "cart-minusbtn".concat(i)
+    );
+    let cartAddbtn = document.getElementsByClassName("cart-addbtn".concat(i));
+    let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
+
     if (event.target.classList.contains("cart-btn".concat(i))) {
-      let cartMinusbtn = document.getElementsByClassName(
-        "cart-minusbtn".concat(i)
-      );
-      let cartAddbtn = document.getElementsByClassName("cart-addbtn".concat(i));
-      cartAddbtn[0].style.display = "block";
-      cartMinusbtn[0].style.display = "block";
-      let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
-      items[i]["quantity"]++;
-      addtocartbtn[0].textContent = items[i]["quantity"];
-      setListItems(items);
-
-      cartQuantity++;
-      if (cartQuantity >= 1) {
-        cartSum.textContent = cartQuantity;
-        cartSum.style.display = "inline-block";
-      }
+      centerCartBtn(cartAddbtn[0], cartMinusbtn[0], addtocartbtn[0], i);
     }
-  }
-});
 
-cardGroup.addEventListener("click", function (event) {
-  for (let i = 0; i < 8; i++) {
     if (event.target.classList.contains("cart-addbtn".concat(i))) {
-      items[i]["quantity"]++;
-      let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
-      addtocartbtn[0].textContent = items[i]["quantity"];
-      setListItems(items);
-      cartQuantity++;
-      if (cartQuantity >= 1) {
-        cartSum.textContent = cartQuantity;
-        cartSum.style.display = "inline-block";
-      }
+      addCartBtn(addtocartbtn[0], i);
     }
-  }
-});
 
-cardGroup.addEventListener("click", function (event) {
-  for (let i = 0; i < 8; i++) {
     if (event.target.classList.contains("cart-minusbtn".concat(i))) {
-      let zero = --items[i]["quantity"];
-      let addtocartbtn = document.getElementsByClassName("cart-btn".concat(i));
-      addtocartbtn[0].textContent = zero;
-      if (zero <= 0) {
-        addtocartbtn[0].textContent = "Add to Cart";
-        zero = 0;
-        let cartMinusbtn = document.getElementsByClassName(
-          "cart-minusbtn".concat(i)
-        );
-        let cartAddbtn = document.getElementsByClassName(
-          "cart-addbtn".concat(i)
-        );
-        if (items[i]["quantity"] < 1) {
-          cartAddbtn[0].style.display = "none";
-          cartMinusbtn[0].style.display = "none";
-        }
-      } else {
-        addtocartbtn[0].textContent = zero;
-      }
-      cartQuantity--;
-      if (cartQuantity >= 1) {
-        cartSum.textContent = cartQuantity;
-        cartSum.style.display = "inline-block";
-      } else {
-        cartSum.style.display = "none";
-      }
-      setListItems(items);
+      minusCartBtn(addtocartbtn[0], cartAddbtn[0], cartMinusbtn[0], i);
     }
   }
 });
